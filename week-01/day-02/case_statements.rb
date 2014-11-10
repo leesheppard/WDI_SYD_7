b8e16115a56ae1c5317f015643e2ac283b0511c0
Jack Dean Watson-Hamblin <jack.hamblin@generalassemb.ly>
2014-11-11 09:49:53 +1100

    Forgot to save

---
+++ week-01/day-02/case_statements.rb
@@ -0,0 +1,22 @@
+a = "Jack"
+
+if a == "Jack"
+  puts "AWESOME TEACHER!"
+elsif a == "Wolf"
+  puts "AWESOME DEVELOPER IN RESIDENCE"
+elsif a == "John"
+  puts "Something"
+end
+
+grade = "B"
+
+case grade
+when "A"
+  puts "Well done!"
+when "B"
+  puts "try harder"
+when "C"
+  puts "YOU NEED HELP!!!"
+else
+  puts "You're just making it up!"
+end
