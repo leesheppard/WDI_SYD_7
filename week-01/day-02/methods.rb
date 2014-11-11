bc4cf96199185a6cec0e9c660852495a132daacf
Jack Dean Watson-Hamblin <jack.hamblin@generalassemb.ly>
2014-11-11 16:36:25 +1100

    Loops, basic calculator, methods

--- week-01/day-02/methods.rb
+++ week-01/day-02/methods.rb
@@ -11,42 +11,66 @@
 
 # area(2, 2) * 20 #=> 80
 
-def leap_year?(year)
-  year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
-end
+# def leap_year?(year)
+#   year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+# end
 
-def leap_year_check(year)
-  if leap_year? year
-    puts "#{year} is a leap year"
-  else
-    puts "#{year} is not a leap year"
+# def leap_year_check(year)
+#   if leap_year? year
+#     puts "#{year} is a leap year"
+#   else
+#     puts "#{year} is not a leap year"
+#   end
+# end
+
+# leap_year_check(1997)
+# leap_year_check(1996)
+# leap_year_check(1900)
+# leap_year_check(2000)
+
+
+# def square(x)
+#   x * x
+# end
+
+# def volume(length, width, height)
+#   length * width * height
+# end
+
+# def ask_age
+#   puts "What is your age?"
+#   gets.chomp.to_i
+# end
+
+# puts("Your age is : #{ask_age}")
+
+# def puts(string_to_print)
+#   # some crazy stuff
+# end
+
+def what_borrough(suburb)
+  if ["bondi", "newtown", "surry hills"].include?(suburb)
+    puts "Are you a hipster?"
   end
 end
 
-leap_year_check(1997)
-leap_year_check(1996)
-leap_year_check(1900)
-leap_year_check(2000)
+answer = gets.strip
+puts answer
+
+what_borrough(answer)
 
 
-def square(x)
-  x * x
-end
 
-def volume(length, width, height)
-  length * width * height
-end
 
-def ask_age
-  puts "What is your age?"
-  gets.chomp.to_i
-end
 
-puts("Your age is : #{ask_age}")
 
-def puts(string_to_print)
-  # some crazy stuff
-end
+
+
+
+
+
+
+
 
 
 
