e09b56b5fb8bbd587e46f7700647310964184b66
Jack Dean Watson-Hamblin <jack.hamblin@generalassemb.ly>
2014-11-14 11:56:45 +1100

    Availability

--- week-01/day-05/rentals/apartment.rb
+++ week-01/day-05/rentals/apartment.rb
@@ -1,13 +1,13 @@
 class Apartment
   attr_accessor :price,
-                :is_occupied,
+                :is_available,
                 :square_foot,
                 :number_of_bedrooms,
                 :number_of_bathrooms
 
-  def initialize(price, is_occupied, square_foot, number_of_bedrooms, number_of_bathrooms)
+  def initialize(price, is_available, square_foot, number_of_bedrooms, number_of_bathrooms)
     self.price = price
-    self.is_occupied = is_occupied
+    self.is_available = is_available
     self.square_foot = square_foot
     self.number_of_bedrooms = number_of_bedrooms
     self.number_of_bathrooms = number_of_bathrooms
@@ -16,7 +16,7 @@ class Apartment
   def to_s
     "Apartment:\n" +
     "  Price: #{price}\n" +
-    "  Is occupied: #{is_occupied}\n" +
+    "  Is available: #{is_available}\n" +
     "  Square Foot: #{square_foot}\n" +
     "  Number of bedrooms: #{number_of_bedrooms}\n" +
     "  Number of bathrooms: #{number_of_bathrooms}\n"
--- week-01/day-05/rentals/main.rb
+++ week-01/day-05/rentals/main.rb
@@ -1,15 +1,17 @@
+require "pry"
+
 require_relative "building"
 require_relative "apartment"
 
 building_one_apartments = [
-  Apartment.new(250.0, true, 400, 1, 1),
   Apartment.new(250.0, false, 400, 1, 1),
-  Apartment.new(450.0, false, 850, 2, 1)
+  Apartment.new(250.0, true, 400, 1, 1),
+  Apartment.new(450.0, true, 850, 2, 1)
 ]
 
 building_two_apartments = [
-  Apartment.new(250.0, true, 400, 1, 1),
   Apartment.new(250.0, false, 400, 1, 1),
+  Apartment.new(250.0, true, 400, 1, 1),
   Apartment.new(450.0, false, 850, 2, 1)
 ]
 
@@ -45,17 +47,39 @@ buildings = [
 
 # my_puts(buildings)
 
-def list_buildings(buildings)
-  buildings.each do |building|
-    puts "#{buildings.index(building)}: #{building}"
+def list_buildings(all_buildings)
+  all_buildings.each do |single_building|
+
+    is_available_values = single_building.apartments.map do |single_apartment|
+      single_apartment.is_available
+    end
+
+    is_there_an_available_apartment = is_available_values.include?(true)
+
+    if is_there_an_available_apartment
+      puts "#{all_buildings.index(single_building)}: #{single_building}"
+    end
   end
+
+  # available_buildings = all_buildings.select do |single_building|
+  #   is_occupied_values = single_building.apartments.map do |single_apartment|
+  #     single_apartment.is_occupied
+  #   end
+
+  #   is_occupied_values.include?(false)
+  # end
+
+  # puts available_buildings
 end
 
-def list_apartments(buildings)
-  buildings.each do |building|
-    building.apartments.each do |apartment|
-      puts apartment
-    end
+def list_apartments(buildings_array)
+  print "Choose a building: "
+  building_number = gets.strip.to_i
+
+  single_building = buildings_array[building_number]
+
+  single_building.apartments.each do |a|
+    puts "#{single_building.apartments.index(a)}: #{a}"
   end
 end
 
