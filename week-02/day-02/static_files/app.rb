884e7d82f5fa85e4abe548fcf2fc6aa45f68d790
Jack Dean Watson-Hamblin <jack.hamblin@generalassemb.ly>
2014-11-18 16:36:37 +1100

    Static files

--- week-02/day-02/static_files/app.rb
+++ week-02/day-02/static_files/app.rb
@@ -4,3 +4,9 @@ require "sinatra/reloader"
 get '/' do
   erb :home, layout: :layout
 end
+
+get '/students/:name' do
+  student_name = params[:name]
+  @the_student = student_data[student_name]
+  erb :student, layout: :layout
+end
