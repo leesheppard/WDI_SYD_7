b0ea2dee4baae45ad3168ab2f6f9db8eff00fc4f
Jack Dean Watson-Hamblin <jack.hamblin@generalassemb.ly>
2015-01-08 17:08:54 +1100

    JS Todos + Rails

---
+++ week-07/day-04/todos/app/assets/javascripts/application.js
@@ -0,0 +1,163 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+
+
+
+
+
+
+$(function() {
+  $("#add-todo").on("keypress", function(event) {
+    if (event.keyCode === 13 && $(this).val() !== "") {
+      $.ajax({
+        method: "POST",
+        url: "/todos",
+        dataType: "json",
+        data: {
+          todo: {
+            text: $(this).val()
+          }
+        },
+        success: function(todo) {
+          $("body").append("<p data-id=\"" + todo.id + "\" class=\"todo\">" + todo.text + "</p>");
+        }
+      });
+    }
+  });
+
+  $("body").on("click", ".todo", function() {
+    var todoURL = "/todos/" + $(this).attr("data-id");
+    // "/todos/11"
+
+    var self = this;
+
+    $.getJSON(todoURL, function(todo) {
+      $(self).append("<div>" + todo.notes + "</div>");
+    });
+  });
+
+  $.getJSON("/todos", function(todos) {
+    $.each(todos, function(index, todo) {
+      $("body").append("<p data-id=\"" + todo.id + "\" class=\"todo\">" + todo.text + "</p>");
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(function() {
+//   $("#add-todo").on("submit", function(event) {
+//     var text = $("#todo-text").val();
+//     var notes = $("#todo-notes").val();
+//     var completed = $("#todo-completed").prop("checked");
+
+//     $.ajax({
+//       url: "/todos",
+      // method: "POST",
+      // dataType: "json",
+      // data: {
+      //   todo: {
+      //     text: text,
+      //     notes: notes,
+      //     completed: completed
+      //   },
+      // },
+    //   success: function(data) {
+    //     console.log(data);
+    //   },
+    //   failure: function(data) {
+
+    //   }
+    // })
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // $.params({ todo:
+    //     { text: text,
+    //       notes: notes,
+    //       completed: completed }
+    //     })
+    // todo[text]=Do the dishes&todo[notes]=Get liquid
+
+    // $.post("/todos/:id",
+    //   { todo:
+    //     { text: text,
+    //       notes: notes,
+    //       completed: completed },
+    //       _method: "PATCH"
+    //     },
+    //     function(data) {
+    //       $("body").append("<p>" + data.text + "</p>");
+    //     },
+    //     "json");
+
+//     event.preventDefault();
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
