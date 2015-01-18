ba86baa4623105aac6e0b3f1c4888582f8e99534
Jack Dean Watson-Hamblin <jack.hamblin@generalassemb.ly>
2015-01-19 10:15:23 +1100

    Get JSON list of statuses and added some seed data

--- week-09/day-01/twitter_clone/app/controllers/statuses_controller.rb
+++ week-09/day-01/twitter_clone/app/controllers/statuses_controller.rb
@@ -1,2 +1,9 @@
 class StatusesController < ApplicationController
+  def index
+    @statuses = Status.all
+
+    respond_to do |f|
+      f.json { render json: @statuses }
+    end
+  end
 end
--- week-09/day-01/twitter_clone/db/seeds.rb
+++ week-09/day-01/twitter_clone/db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Creating some seed statuses"
+Status.create({
+  username: "FluffyJack",
+  content: "Lorem ipsum dolar sit amet"
+})
+
+Status.create({
+  username: "Avdi",
+  content: "Aperiam cum vero, neque similique corporis rem velit ducimus, quia possimus deserunt laudantium."
+})
+
+Status.create({
+  username: "JimboJaggins",
+  content: "Hello Twitter Clone"
+})
