a7cd30c37fa4d9ee4f69b4c06ae7e82f1fdfe695
Jack Dean Watson-Hamblin <jack.hamblin@generalassemb.ly>
2015-01-19 13:08:54 +1100

    Basic CRUD statuses controller and backbone models

--- week-09/day-01/twitter_clone/app/controllers/statuses_controller.rb
+++ week-09/day-01/twitter_clone/app/controllers/statuses_controller.rb
@@ -6,4 +6,36 @@ class StatusesController < ApplicationController
       f.json { render json: @statuses }
     end
   end
+
+  def create
+    @status = Status.create(status_params)
+
+    respond_to do |f|
+      f.json { render json: @status }
+    end
+  end
+
+  def update
+    @status = Status.find(params[:id])
+    @status.update(status_params)
+
+    respond_to do |f|
+      f.json { render json: @status }
+    end
+  end
+
+  def destroy
+    @status = Status.find(params[:id])
+    @status.destroy
+
+    respond_to do |f|
+      f.json { render json: { status: "ok" } }
+    end
+  end
+
+  private
+
+  def status_params
+    params.require(:status).permit(:username, :content)
+  end
 end
