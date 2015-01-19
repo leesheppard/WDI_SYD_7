4a8be5a08d3a8dd1205099f16325db45fb2f392b
Jack Dean Watson-Hamblin <jack.hamblin@generalassemb.ly>
2015-01-19 13:54:54 +1100

    Examples in the application.js file

--- week-09/day-01/twitter_clone/app/assets/javascripts/application.js
+++ week-09/day-01/twitter_clone/app/assets/javascripts/application.js
@@ -15,3 +15,41 @@
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree .
+
+// Create a collection, starts empty
+var statusesCollection = new Statuses();
+
+console.log(statusesCollection.length); // 0
+
+// Listen for when statuses are added to the collection
+statusesCollection.on("add", function(status) {
+  console.log("Status added: " + status.get('content'));
+});
+
+// Load all statuses from the defined URL - GET /statuses.json
+statusesCollection.fetch().then(function() {
+
+  console.log(statusesCollection.length); // 3
+
+  // Get the first status from the collection
+  var firstStatus = statusesCollection.at(0);
+
+  // Change its content
+  firstStatus.set({ content: "Test test test" });
+
+  // Save it to the server - PUT /statuses/:id
+  firstStatus.save(); // `statusesCollection.sync()` would have achieved the same thing
+
+  // Create new statuses
+  statusesCollection.create({ username: "FluffyJack", content: "Foo bar foo bar" });
+  statusesCollection.create({ username: "FluffyJack", content: "Learn you some backbone.js" });
+
+  // Create a new status outside of the collection
+  var myStatus = new Status();
+  myStatus.set({ username: "FluffyJack", content: "These words are my own!" });
+  myStatus.save(); // POST /statuses
+
+  // Destroy a status
+  myStatus.destroy(); // DELETE /statuses/:id
+
+});
