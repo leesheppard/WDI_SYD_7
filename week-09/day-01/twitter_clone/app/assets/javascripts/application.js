5d5be34cb25299f6cc8aa2149cc647c71d5a3f37
Jack Dean Watson-Hamblin <jack.hamblin@generalassemb.ly>
2015-01-20 12:06:47 +1100

    Rendering out the statuses with hbs templates

--- week-09/day-01/twitter_clone/app/assets/javascripts/application.js
+++ week-09/day-01/twitter_clone/app/assets/javascripts/application.js
@@ -12,44 +12,46 @@
 //
 //= require jquery_ujs
 //= require turbolinks
+//= require handlebars.runtime
+//= require_tree ./templates
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree .
 
-// // Create a collection, starts empty
-// var statusesCollection = new Statuses();
+var statusCollection = new Statuses();
 
-// console.log(statusesCollection.length); // 0
+statusCollection.on("add", function(status) {
+  $("body").append(
+    JST['status'](
+      status.toJSON()
+    )
+  );
+});
 
-// // Listen for when statuses are added to the collection
-// statusesCollection.on("add", function(status) {
-//   console.log("Status added: " + status.get('content'));
-// });
+statusCollection.fetch();
 
-// // Load all statuses from the defined URL - GET /statuses.json
-// statusesCollection.fetch().then(function() {
+statusCollection.create({
+  username: "JackFluff", // $(".username").val()
+  content: "FLUFFY THE THREE HEADED DOG!" // $(".status-text").val()
+});
 
-//   console.log(statusesCollection.length); // 3
 
-//   // Get the first status from the collection
-//   var firstStatus = statusesCollection.at(0);
 
-//   // Change its content
-//   firstStatus.set({ content: "Test test test" });
 
-//   // Save it to the server - PUT /statuses/:id
-//   firstStatus.save(); // `statusesCollection.sync()` would have achieved the same thing
 
-//   // Create new statuses
-//   statusesCollection.create({ username: "FluffyJack", content: "Foo bar foo bar" });
-//   statusesCollection.create({ username: "FluffyJack", content: "Learn you some backbone.js" });
 
-//   // Create a new status outside of the collection
-//   var myStatus = new Status();
-//   myStatus.set({ username: "FluffyJack", content: "These words are my own!" });
-//   myStatus.save(); // POST /statuses
 
-//   // Destroy a status
-//   myStatus.destroy(); // DELETE /statuses/:id
 
-// });
+
+
+
+
+
+
+
+
+
+
+
+
+
