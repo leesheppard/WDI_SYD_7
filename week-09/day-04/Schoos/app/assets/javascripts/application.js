fc5aad8a2d6b4ccb261d3c5cfdedd20162518cc9
Jack Dean Watson-Hamblin <jack.hamblin@generalassemb.ly>
2015-01-22 15:08:16 +1100

    Now using non-CDN external assets
    * Removed external references from layout
    * Downloaded dependencies to vendor
    * Returned jasmine config to default

--- week-09/day-04/Schoos/app/assets/javascripts/application.js
+++ week-09/day-04/Schoos/app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require jquery_ujs
+//= require underscore
+//= require backbone
 //= require turbolinks
 //= require handlebars.runtime
 //= require_tree .
