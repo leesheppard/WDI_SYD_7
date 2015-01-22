b8e7a4390333dda2a99563aacdc6fb842c0f8404
Jack Dean Watson-Hamblin <jack.hamblin@generalassemb.ly>
2015-01-22 15:13:33 +1100

    Generated school resource
    * Has name and description
    * Generated model
    * Generated controller
    * Empty specs

--- week-09/day-04/Schoos/db/schema.rb
+++ week-09/day-04/Schoos/db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150122041146) do
+
+  create_table "schools", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
